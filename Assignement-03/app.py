44f6c894443d78c001505ccc24d408db9d310d85
Akshay Kumar T <106388941+akshaykumart@users.noreply.github.com>
2025-01-22 23:10:38 +0530

    Update app.py

--- Assignement-03/app.py
+++ Assignement-03/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-import os
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # Exposes metrics at /metrics
 
 @app.route('/')
 def hello():
-    return "Hello, Kubernetes!"
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
